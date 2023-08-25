62501b5a2946d9585e7f4d6bdca0190c946a3d1c
Tony Chen <yang.chen2@anu.edu.au>
2023-08-25 14:32:54 +1000

    Update project.

---
+++ src/backend/Trust-Indicator/Model/Photo.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TrustIndicator.Model
+{
+    public class Photo
+    {
+        [Key]
+        public int PhotoID { get; set; }
+        [Required]
+        public int UserID { get; set; }
+        public string PhotoName { get; set; }
+        [Required]
+        public string PhotoUrl { get; set;}
+        public string? PhotoDescription { get; set;}
+        [Timestamp]
+        public byte[] UploadDate { get; set; }
+        public string Tag { get; set; }
+    }
+}
